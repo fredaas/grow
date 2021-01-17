d285ae7b071c4ce479903cf11d59542c7046bd2e
Fredrik Ås <fredaas86@gmail.com>
2021-01-17 21:05:30 +0100

    Add unit test

--- utest/clean.sh
+++ utest/clean.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-ls | egrep -v "tree1.txt|tree2.txt|clean.sh|Makefile" | xargs -I{} rm -rf {}
+ls | egrep -v "tree1.txt|tree2.txt|tree3.txt|clean.sh|Makefile" | xargs -I{} rm -rf {}
